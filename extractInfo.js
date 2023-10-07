69f64b51035586798594f42fde2b2dc1510a5b22
asnasim144 <93252806+asnasim144@users.noreply.github.com>
2023-10-07 13:24:30 +0600

    find frame algorithm

--- extractInfo.js
+++ extractInfo.js
@@ -3,17 +3,18 @@ const canvas = document.getElementById('imageCanvas')
 console.log("🚀 ~ file: extractInfo.js:3 ~ canvas:", canvas)
 const form = document.getElementById('form')
 const context = canvas.getContext('2d')
+let imgPixels = []
 const heigth = 150
 const width = 150
-console.log('start')
+let count = 0
+let startingPixel = {}
+
 form.onsubmit = (e) => {
     e.preventDefault()
-    console.log('onsubmit')
     const file = inputImage.files[0]
     console.log(file)
     if(file){
         const reader = new FileReader()
-        console.log('onsubmit')
         reader.onload = (event) => {
             const image = new Image()
             image.src = event.target.result
@@ -27,8 +28,6 @@ form.onsubmit = (e) => {
                 for(let i=0; i < imageData.data.length; i+= 4){
                     alphas.push(imageData.data[i + 3])
                 }
-
-                let imgPixels = []
                 let idx = 0
                 for(let i=0; i < heigth; i++) {
                     imgPixels.push([])
@@ -36,9 +35,38 @@ form.onsubmit = (e) => {
                         imgPixels[i].push( alphas[idx++] < 50 ? 0 : 1)
                     }
                 }
-                console.log(imgPixels)
+                startingPixel = findAPixelOfAFrame(imgPixels)
+                findFrame(startingPixel)
             }
         }
         reader.readAsDataURL(file)
     }
 }
+
+function findAPixelOfAFrame (array) {
+    for(let i=0; i < array[0].length; i++) {
+        for(let j=0; j < array.length; j++) {
+            if(array[i][j] === 1) {
+                // count++
+                console.log(array[i][j])
+                return {
+                    row: i,
+                    col: j,
+                }
+            }
+        }
+    }
+}
+
+function findFrame (currentPixel) {
+    const { row, col } = currentPixel
+    count++
+    console.log(currentPixel)
+    if(count > 150 && currentPixel === startingPixel) return 
+    if(count > 2000) return
+    if(col < imgPixels[0].length) findFrame({row, col: col + 1})
+    if(row < imgPixels.length) findFrame({row: row + 1, col})
+    if(col > 0) findFrame({row, col: col - 1})
+    if(row > 0) findFrame({row: row - 1, col})
+}
+
